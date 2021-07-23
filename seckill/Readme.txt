deaa44bc9d40d4601e19e455f7640311b99b960f
Shijie.Cao <48547088+silence11207@users.noreply.github.com>
2021-07-23 12:13:46 +0800

    Delete seckill directory

--- seckill/Readme.txt
+++
@@ -1,9 +0,0 @@
-
-原型界面：./v0/seckill-site-demo
-
-SQL文件：./v0/sql
-
-后台代码：./v(1-3)/seckill
-
-前台代码：./v(1-3)/seckill-site
-
--- seckill/v3/seckill/src/main/java/com/nowcoder/seckill/service/PromotionService.java
+++
@@ -1,7 +0,0 @@
-package com.nowcoder.seckill.service;
-
-public interface PromotionService {
-
-    String generateToken(int userId, int itemId, int promotionId);
-
-}
